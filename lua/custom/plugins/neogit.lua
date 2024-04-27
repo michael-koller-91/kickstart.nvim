b85e335df7f041b866cf8327fb06089feb045326
Michael Koller <koller_michael@outlook.de>
2024-04-27 16:47:47 +0200

    add lua/custom/plugins/neogit.lua

---
+++ lua/custom/plugins/neogit.lua
@@ -0,0 +1,15 @@
+-- A git interface for Neovim, inspired by Magit.
+-- https://github.com/NeogitOrg/neogit
+
+return {
+  'NeogitOrg/neogit',
+  dependencies = {
+    'nvim-lua/plenary.nvim', -- required
+    'sindrets/diffview.nvim', -- optional - Diff integration
+
+    -- Only one of these is needed, not both.
+    'nvim-telescope/telescope.nvim', -- optional
+    'ibhagwan/fzf-lua', -- optional
+  },
+  config = true,
+}
