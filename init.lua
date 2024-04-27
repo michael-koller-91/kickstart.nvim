06d7d0a4569ab83627c64ab26eafb996ddefbc14
Michael Koller <koller_michael@outlook.de>
2024-04-27 22:08:21 +0200

    run VHDL formatter on *.vhd save

--- init.lua
+++ init.lua
@@ -209,6 +209,10 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
+-- Format VHDL files on save.
+local group = vim.api.nvim_create_augroup('VHDL-Formatter', { clear = true })
+vim.api.nvim_create_autocmd('BufWritePost', { command = ':!vsg --configuration $VSGCONFIG -f <afile> --fix', pattern = { '*.vhd' }, group = group })
+
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
