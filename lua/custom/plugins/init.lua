6e5a5a77d1242da01162a8ab12719db72cfa1a18
Michael Koller <michael.koller@rohde-schwarz.com>
2024-05-08 09:04:29 +0200

    update lua/custom/plugins/init.lua: add vim-table-mode

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,8 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  -- An awesome automatic table creator & formatter allowing one to create neat tables as you type.
+  -- https://github.com/dhruvasagar/vim-table-mode
+  'dhruvasagar/vim-table-mode',
+}
