f9cbd5ccb3d150e86c047734c014bc0a5e55a6c4
Michael Koller <koller_michael@outlook.de>
2024-05-20 16:02:27 +0200

    update init.lua: adjust description of keymap

--- init.lua
+++ init.lua
@@ -441,7 +441,7 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
       vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
-      vim.keymap.set('n', '<leader>so', '<cmd>Telescope heading<cr>', { desc = '[S]earch Through [O]rg Headings' })
+      vim.keymap.set('n', '<leader>so', '<cmd>Telescope heading<cr>', { desc = '[S]earch [O]rg Headings' })
 
       -- Slightly advanced example of overriding default behavior and theme
       vim.keymap.set('n', '<leader>/', function()
